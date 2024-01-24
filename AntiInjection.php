ea9dcbc8d26a6883a7aaa44687a127cdfc232f60
User User <useruser1985official@outlook.com>
2024-01-24 10:03:17 -0300

    Atualização

--- AntiInjection.php
+++ AntiInjection.php
@@ -1,48 +1,47 @@
 <?php
 /*
-* Uso: Criar objeto dessa classe e usar os métodos públicos, por exemplo:
+* Uso: Usar os métodos públicos estáticos da classe, por exemplo:
 *
-* $anti = new AntiInjection();
-*
-* $anti->campo($login);
-* $anti->campo($senha);
+* $l = AntiInjection::campo($login);
+* $s = AntiInjection::campo($senha);
 *
 * O método texto é pra áreas de texto, o campo é para inputs como text e password, o numero é para números inteiros.
 *
 * A classe limpa strings para evitar SQL Injection e Ataques XSS.
 */
 
+<?php
 class AntiInjection {
-    private $limpa;
-    private $inteiro;
+    private static $limpa;
+    private static $inteiro;
     private static $listaNegra = array("select", "update", "drop", "truncate", "insert", "delete", "alter", "from", "where", "table", "tables", "database", "union", "--", "%", "<", ">", "[", "]", ":", "?", "`", "|", "*");
 
-    public function texto($frase) : string {
-        $this->limpa = str_ireplace(";", "&#59;", $frase);
-        $this->limpa = str_ireplace("--", "&#45;&#45;", $this->limpa);
-        $this->limpa = str_ireplace("*", "&#42;", $this->limpa);
-        $this->limpa = str_ireplace("=", "&#61;", $this->limpa);
-        $this->limpa = htmlentities($this->limpa, ENT_QUOTES);
-        $this->limpa = str_ireplace("amp;", "", $this->limpa);
-        $this->limpa = str_ireplace("&&", "&amp;&amp;", $this->limpa);
-        $this->limpa = str_ireplace("||", "&#124;&#124;", $this->limpa);
-        $this->limpa = str_ireplace("!", "&#33;", $this->limpa);
-        $this->limpa = strip_tags($this->limpa);
-        $this->limpa = trim($this->limpa);
+    public static function texto($frase) : string {
+        self::$limpa = str_ireplace(";", "&#59;", $frase);
+        self::$limpa = str_ireplace("--", "&#45;&#45;", self::$limpa);
+        self::$limpa = str_ireplace("*", "&#42;", self::$limpa);
+        self::$limpa = str_ireplace("=", "&#61;", self::$limpa);
+        self::$limpa = htmlentities(self::$limpa, ENT_QUOTES);
+        self::$limpa = str_ireplace("amp;", "", self::$limpa);
+        self::$limpa = str_ireplace("&&", "&amp;&amp;", self::$limpa);
+        self::$limpa = str_ireplace("||", "&#124;&#124;", self::$limpa);
+        self::$limpa = str_ireplace("!", "&#33;", self::$limpa);
+        self::$limpa = strip_tags(self::$limpa);
+        self::$limpa = trim(self::$limpa);
 
-        return $this->limpa;
+        return self::$limpa;
     }
 
-    public function campo($frase) : string {
-        $this->limpa = $this->texto($frase);
-        $this->limpa = str_ireplace(self::$listaNegra, "", $this->limpa);
+    public static function campo($frase) : string {
+        self::$limpa = self::texto($frase);
+        self::$limpa = str_ireplace(self::$listaNegra, "", self::$limpa);
 
-        return $this->limpa;
+        return self::$limpa;
     }
 
-    public function numero($num) : int {
-        $this->inteiro = (int)$this->campo($num);
+    public static function numero($num) : int {
+        self::$inteiro = (int)self::campo($num);
 
-        return $this->inteiro;
+        return self::$inteiro;
     }
 }
